ec5741f6f078c65d0c4d4c2a7a1585515f0cc005
mhbaando <maxamuudyare21@gmail.com>
2022-01-13 18:37:09 +0300

    first Commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// function viralAdvertising(n) {
+//   let shared = 5;
+//   let liked = 2;
+//   let cumulative = 2;
+
+//   for (let i = 2; i <= n; i++) {
+//     shared = liked * 3;
+//     liked = Math.floor(shared / 2);
+//     cumulative += liked;
+//   }
+//   return cumulative;
+// }
+
+// console.log(viralAdvertising(5));
